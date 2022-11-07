5d661dd0eb34f9ea006eff3941a1777a506eb816
sxhvxa <muhammadshax07@gmail.com>
2022-11-07 20:04:15 +0700

    add register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,13 @@ const loginUser = async (req, res) => {
 };
 
 const registerUser = async (req, res) => {
-  res.json({ mssg: "register" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, registerUser };
--- models/userModels.js
+++ models/userModels.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -13,4 +13,15 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email sudah digunakan");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 module.exports = mongoose.model("User", userSchema);
