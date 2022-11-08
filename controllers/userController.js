8051878eab51f49b1e1ff6450713f9a4c0e41882
sxhvxa <muhammadshax07@gmail.com>
2022-11-08 19:17:49 +0700

    add token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require("../models/userModels");
 const jwt = require("jsonwebtoken");
-
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "7d" });
+};
 const loginUser = async (req, res) => {
   res.json({ mssg: "login" });
 };
@@ -9,7 +11,8 @@ const registerUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
