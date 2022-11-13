0698e1a9838a9d12170e77a9d4fe52bf7ff13555
sxhvxa <muhammadshax07@gmail.com>
2022-11-13 20:51:27 +0700

    log some id

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -8,6 +8,7 @@ const requireAuth = async (req, res, next) => {
   const token = authorization.split(" ")[1];
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
+    console.log(_id);
     req.user = await User.findOne({ _id }).select("_id");
     next();
   } catch (error) {
